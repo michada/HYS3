6113db9147002b1552cace4693efdd1f44da71c9
ansousa <ansousa@esei.uvigo.es>
2015-04-27 01:31:29 +0200

    Eliminado UsuariosDAOTest.java del buildpath, eliminado el import y el uso del mismo en PeopleTestSuite.java

--- src/test/java/es/uvigo/esei/daa/PeopleTestSuite.java
+++ src/test/java/es/uvigo/esei/daa/PeopleTestSuite.java
@@ -4,11 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import es.uvigo.esei.daa.dao.UsuariosDAOTest;
+//import es.uvigo.esei.daa.dao.UsuariosDAOTest;
 import es.uvigo.esei.daa.rest.EventosTest;
 import es.uvigo.esei.daa.web.EventosWebTest;
 
-@SuiteClasses({ UsuariosDAOTest.class, EventosTest.class, EventosWebTest.class })
+@SuiteClasses({ /*UsuariosDAOTest.class,*/ EventosTest.class, EventosWebTest.class })
 @RunWith(Suite.class)
 public class PeopleTestSuite {
 }
