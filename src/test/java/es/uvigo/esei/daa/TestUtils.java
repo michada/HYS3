014f86622c7ede880fe3699c337fd08505cb4823
Peter <peter-puessi@hotmail.com>
2015-04-27 19:14:18 +0200

    Arreglo DB

--- src/test/java/es/uvigo/esei/daa/TestUtils.java
+++ src/test/java/es/uvigo/esei/daa/TestUtils.java
@@ -13,36 +13,33 @@ import javax.ws.rs.core.Response;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.mock.jndi.SimpleNamingContextBuilder;
 
-
 public final class TestUtils {
-	private TestUtils() {}
+	private TestUtils() {
+	}
 
-	//Arreglar
+	// Arreglar
 	public static void createFakeContext() throws NamingException {
 		final SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
-		builder.bind("java:/comp/env/jdbc/haveyouseen_db", createTestingDataSource());
+		builder.bind("java:/comp/env/jdbc/HYSDB", createTestingDataSource());
 		builder.activate();
 	}
-	
-	//Arrelgar
+
+	// Arrelgar
 	private static BasicDataSource createTestingDataSource() {
 		final BasicDataSource ds = new BasicDataSource();
 		ds.setDriverClassName("com.mysql.jdbc.Driver");
-		ds.setUrl("jdbc:mysql://localhost:3306/haveyouseen_db?allowMultiQueries=true");
-		ds.setUsername("Admin");
+		ds.setUrl("jdbc:mysql://localhost:3306/HYSDB?allowMultiQueries=true");
+		ds.setUsername("root");
 		ds.setPassword("admin");
 		ds.setMaxActive(100);
 		ds.setMaxIdle(30);
 		ds.setMaxWait(10000);
 		return ds;
 	}
-	
+
 	public static void clearTestDatabase() throws SQLException {
-		final String queries = new StringBuilder()
-			.append("DELETE FROM `Eventos`;")
-			.append("DELETE FROM `Usuarios`;")
-			//.append("DELETE FROM `asistentes`;")
-		.toString();
+		final String queries = new StringBuilder().append(
+				"DELETE FROM `Eventos`;").toString();
 
 		final DataSource ds = createTestingDataSource();
 		try (Connection connection = ds.getConnection()) {
@@ -51,24 +48,14 @@ public final class TestUtils {
 			}
 		}
 	}
-	
+
 	public static void initTestDatabase() throws SQLException {
 		final String queries = new StringBuilder()
-			.append("ALTER TABLE `Usuarios` AUTO_INCREMENT = 1;")
-			.append("ALTER TABLE `Eventos` AUTO_INCREMENT = 1;")
-			.append("INSERT INTO `Usuarios` (`idUsuario`,`login`,`password`,`nombre`) VALUES ('0', 'UsuarioPrueba1','prueba1', 'Pepe');")
-			.append("INSERT INTO `Usuarios` (`idUsuario`,`login`,`password`,`nombre`) VALUES ('0', 'UsuarioPrueba2','prueba2', 'Maria');")
-			.append("INSERT INTO `Usuarios` (`idUsuario`,`login`,`password`,`nombre`) VALUES ('0', 'UsuarioPrueba3','prueba3', 'Jose');")
-			.append("INSERT INTO `Eventos` (`idEvento`,`titulo`,`usuario`,`maxAsistentes`,`inicio`,`fin`) VALUES (0, 'Evento numero 1', 0, '3', '03/05/2015', '09/05/2015');")
-			.append("INSERT INTO `Eventos` (`idEvento`,`titulo`,`usuario`,`maxAsistentes`,`inicio`,`fin`) VALUES (0, 'Evento numero 2', 1, '2', '03/05/2015', '09/05/2015');")
-			.append("INSERT INTO `Eventos` (`idEvento`,`titulo`,`usuario`,`maxAsistentes`,`inicio`,`fin`) VALUES (0, 'Evento numero 3', 2, '10', '03/05/2015', '09/05/2015');")
-//			.append("INSERT INTO `asistentes` (`usuario`,`evento`) VALUES ('UsuarioPrueba1','Evento numero 1');")
-//			.append("INSERT INTO `asistentes` (`usuario`,`evento`) VALUES ('UsuarioPrueba2','Evento numero 1');")
-//			.append("INSERT INTO `asistentes` (`usuario`,`evento`) VALUES ('UsuarioPrueba3','Evento numero 1');")
-//			.append("INSERT INTO `asistentes` (`usuario`,`evento`) VALUES ('UsuarioPrueba1','Evento numero 2');")
-//			.append("INSERT INTO `asistentes` (`usuario`,`evento`) VALUES ('UsuarioPrueba2','Evento numero 2');")
-//			.append("INSERT INTO `asistentes` (`usuario`,`evento`) VALUES ('UsuarioPrueba3','Evento numero 3');")
-		.toString();
+				.append("ALTER TABLE `Eventos` AUTO_INCREMENT = 1;")
+				.append("INSERT INTO `Eventos` (`idEvento`,`titulo`,`usuario`,`maxAsistentes`,`inicio`,`fin`) VALUES (0, 'Evento numero 1', 0, '3', '03/05/2015', '09/05/2015');")
+				.append("INSERT INTO `Eventos` (`idEvento`,`titulo`,`usuario`,`maxAsistentes`,`inicio`,`fin`) VALUES (0, 'Evento numero 2', 1, '2', '03/05/2015', '09/05/2015');")
+				.append("INSERT INTO `Eventos` (`idEvento`,`titulo`,`usuario`,`maxAsistentes`,`inicio`,`fin`) VALUES (0, 'Evento numero 3', 2, '10', '03/05/2015', '09/05/2015');")
+				.toString();
 
 		final DataSource ds = createTestingDataSource();
 		try (Connection connection = ds.getConnection()) {
@@ -79,10 +66,13 @@ public final class TestUtils {
 	}
 
 	public static void assertOkStatus(final Response response) {
-		assertEquals("Unexpected status code", Response.Status.OK.getStatusCode(), response.getStatus());
+		assertEquals("Unexpected status code",
+				Response.Status.OK.getStatusCode(), response.getStatus());
 	}
 
 	public static void assertBadRequestStatus(final Response response) {
-		assertEquals("Unexpected status code", Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+		assertEquals("Unexpected status code",
+				Response.Status.BAD_REQUEST.getStatusCode(),
+				response.getStatus());
 	}
 }
